75b594b97eef77867eed80c73b8483970d4a095c
pro100kryto <pro100kryto.original@gmail.com>
2022-07-02 23:27:44 -0300

    v1 Refactoring

--- src/main/java/com/pro100kryto/server/modules/crypt/connection/ICryptModuleConnection.java
+++ src/main/java/com/pro100kryto/server/modules/crypt/connection/ICryptModuleConnection.java
@@ -7,35 +7,17 @@ import java.util.Arrays;
 
 public interface ICryptModuleConnection extends IModuleConnection {
 
-    int getCryptLen() throws RemoteException;
+    byte[] crypt(byte[] data, byte[] salt) throws RemoteException;
 
-    byte[] crypt(byte[] cryptOut, byte[] data, byte[] salt) throws RemoteException;
-    default byte[] crypt(byte[] data, byte[] salt) throws RemoteException {
-        return crypt(new byte[getCryptLen()], data, salt);
-    }
-    default boolean check(byte[] cryptOut, byte[] crypt, byte[] data, byte[] salt) throws RemoteException {
-        return Arrays.equals(
-                crypt,
-                crypt(cryptOut, data, salt)
-        );
-    }
     default boolean check(byte[] crypt, byte[] data, byte[] salt) throws RemoteException {
         return Arrays.equals(
                 crypt,
                 crypt(data, salt)
         );
     }
-    byte[] decrypt(byte[] dataOut, byte[] crypt, byte[] salt) throws RemoteException;
+    byte[] decrypt(byte[] crypt, byte[] salt) throws RemoteException;
 
-    int getSaltLen() throws RemoteException;
+    byte[] randomSalt(int len) throws RemoteException;
 
-    byte[] randomSalt(byte[] saltOut) throws RemoteException;
-    default byte[] randomSalt() throws RemoteException {
-        return randomSalt(new byte[getSaltLen()]);
-    }
-
-    byte[] combineSalt(byte[] saltOut, byte[] salt1, byte[] salt2) throws RemoteException;
-    default byte[] combineSalt(byte[] salt1, byte[] salt2) throws RemoteException {
-        return combineSalt(new byte[getSaltLen()], salt1, salt2);
-    }
+    byte[] combine(byte[] arr1, byte[] arr2) throws RemoteException;
 }
